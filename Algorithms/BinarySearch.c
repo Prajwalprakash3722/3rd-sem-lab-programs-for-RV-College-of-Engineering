d416fcfe5c189fd2924b36ecfe650d5ff30a1d4a
Prajwal P <prajwalprakash3722@gmail.com>
2021-10-23 00:22:04 +0530

    [update] Binary Search

---
+++ Algorithms/BinarySearch.c
@@ -0,0 +1,33 @@
+// Binary Search in C
+#include <stdio.h>
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l)
+    {
+        int mid = l + (r - l) / 2;
+
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+
+        return binarySearch(arr, mid + 1, r, x);
+    }
+    return -1;
+}
+
+int main(void)
+{
+    int arr[] = {2, 3, 4, 10, 40}, x;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Enter Element to search\n");
+    scanf("%d", &x);
+    int result = binarySearch(arr, 0, n - 1, x);
+    if (result == -1)
+    {
+        printf("Element is not present in array\n");
+    }
+    printf("Element is present at index %d\n", result + 1);
+    return 0;
+}
